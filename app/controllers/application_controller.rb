981a501d9d909c09488c5110c738cc42fc69df83
acgill <alisongill@uchicago.edu>
2022-04-22 00:06:31 +0000

    V1

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,24 @@
 class ApplicationController < ActionController::Base
+  def blank_square_form
+    render({ :template => "calc_templates/square_form.html.erb" })
+  end
+  def calc_square
+    # params is a string
+    @num = params.fetch("elephant").to_f
+    @square_of_num = @num * @num
+    render({ :template => "calc_templates/square_results.html.erb" })
+  end
 
+  def blank_random_form
+    render({ :template => "calc_templates/random_form.html.erb" })
+  end
+  def calc_random
+    # params
+    @lower = params.fetch("user_min").to_f
+    @upper = params.fetch("user_max").to_f
+    @result = rand(@lower..@upper)
+
+    render({ :template => "calc_templates/random_results.html.erb"})
+  end
 end
+
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,19 @@
 Rails.application.routes.draw do
+  #square
+  get("/square/new", { :controller => "application", :action => "blank_square_form" })
+  get("/square/results", { :controller => "application", :action => "calc_square" })
+  
+  #square root
+  get("/square_root/new", { :controller => "application", :action => "blank_root_form" })
+  get("/square_root/results", { :controller => "application", :action => "calc_root" })
+
+  #payment
+  get("/payment/new", { :controller => "application", :action => "blank_payment_form" })
+  get("/payment/results", { :controller => "application", :action => "calc_payment" })
+
+  #random
+  get("/random/new", { :controller => "application", :action => "blank_random_form" })
+  get("/random/results", { :controller => "application", :action => "calc_random" })
 
 end
+
